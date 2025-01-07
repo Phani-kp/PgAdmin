b41a979a501d1868b13d44b84fe20b32c4d71be9
Phani-kp <Phaneendhrak0427@gmail.com>
2025-01-06 20:33:57 -0600

    Add files via upload

---
+++ Q2.sql
@@ -0,0 +1,3 @@
+SELECT * FROM country
+where country like '%n'
+-- The '%' wildcard matches any sequence of characters ending with 'n'
---
+++ Q3.sql
@@ -0,0 +1,3 @@
+Select * from category
+where name like '%el%'
+-- The '%' wildcard matches only sequence of characters having 'el'
