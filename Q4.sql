6d7e479ee0aabc5a102148bc1c266dec38a81d5a
Phani-kp <Phaneendhrak0427@gmail.com>
2025-01-08 18:14:49 -0600

    Add files via upload

---
+++ Q4.sql
@@ -0,0 +1,4 @@
+Select max(replacement_cost) from film;
+
+-- This query retrieves the highest value of the 'replacement_cost' column from the 'film' table.
+-- It uses the aggregate function MAX() to calculate the maximum replacement cost among all records.
