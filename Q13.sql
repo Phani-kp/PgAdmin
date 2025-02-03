c1c97852b33158c78b9e1c97fb8eb09bf4c4cbec
Phani-kp <Phaneendhrak0427@gmail.com>
2025-02-03 15:40:44 -0600

    Add files via upload

---
+++ Q13.sql
@@ -0,0 +1,2 @@
+SELECT * FROM ACTOR 
+WHERE FIRST_NAME LIKE 'A%'; --A% represents that string which start with capital A
---
+++ q12.sql
@@ -0,0 +1,3 @@
+SELECT * FROM ACTOR 
+WHERE FIRST_NAME LIKE '%e'; --like is case-sensitive, we can also use ILIKE which is case insensitive
+--%e represents that string which ends with small e
