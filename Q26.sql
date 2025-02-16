f7e4ebd5b3a0512d42e98d1441f573449bba4e0f
Phani-kp <Phaneendhrak0427@gmail.com>
2025-02-16 12:12:42 -0600

    DDL - DATA DEFINATION LANGUAGE

---
+++ Q26.sql
@@ -0,0 +1,3 @@
+CREATE TABLE EMPLOYEE(Employeeid VARCHAR(255) PRIMARY KEY,
+FirstName CHAR(50),
+LastName CHAR(50));  -- Synstax to create a  table
---
+++ Q27.sql
@@ -0,0 +1,8 @@
+INSERT INTO EMPLOYEE VALUES ('001','Donald','MC'), -- sysntax to insert values in table
+('002','John','Wick'),
+('003','Arnold','Klint'),
+('004','Paul','Ron'),
+('005','Herma','Harry');
+
+SELECT * FROM EMPLOYEE;
+
